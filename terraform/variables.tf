5eb96ec994fda6cf208d003a699b04bcf18e5a9c
tsb04 <twizfouiny@gmail.com>
2025-05-29 14:46:34 +0000

    fixed git action variables  location & settings

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,9 @@
 variable "public_key_path" {
-  default = "~/.ssh/id_rsa.pub"
+  description = "Path to the public key file"
+  type        = string
 }
 
 variable "private_key_path" {
-  default = "~/.ssh/id_rsa"
+  description = "Path to the private key file"
+  default     = "~/.ssh/id_rsa"
 }
