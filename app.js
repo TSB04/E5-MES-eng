2ad11231d3962c47df29fb4254ca8ca872dd68d3
tsb04 <twizfouiny@gmail.com>
2025-05-29 12:17:30 +0000

    Add jest test and optimized gitignore

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const tasksRoutes = require('./routes/tasks');
+const express = require("express");
+const tasksRoutes = require("./routes/tasks");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/tasks', tasksRoutes);
+app.use("/tasks", tasksRoutes);
 
-app.listen(port, () => {
-  console.log(`ToDo API listening at http://localhost:${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`ToDo API listening at http://localhost:${port}`);
+  });
+}
+
+module.exports = app; // Export the app for testing purposes
